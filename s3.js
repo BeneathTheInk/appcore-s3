b212aa6227124e016c1e48aba86ff865ecbf935b
Tyler Johnson <tyler@vintyge.com>
2015-05-01 17:45:48 -0600

    now an adaptor for appcore-files

--- s3.js
+++ s3.js
@@ -1,47 +1,25 @@
 var knox = require("knox"),
-	app = require("./"),
-	mime = require("mime"),
-	async = require("async"),
 	Promise = require("bluebird"),
-	_ = require("underscore"),
-	toStream = require('streamifier').createReadStream;
+	_ = require("underscore");
 
 module.exports = function() {
-	if (this.uploadToS3) return;
+	if (this.knox) return;
 
-	var knox_opts = this.get("s3");
+	var knox_opts = this.get("s3") || this.get("files.s3");
 	if (!knox_opts) throw new Error("Missing S3 configuration.");
 
 	// create a knox client form options
-	var client = knox.createClient(knox_opts);
+	var client = this.knox = knox.createClient(knox_opts);
 
-	// file upload queue so S3 connection doesn't bottleneck
-	var queue = async.queue(function(task, callback) {
-		upload(task.data, task.path, task.options)
-		.then(function(url) { task.url = url; })
-		.nodeify(callback);
-	}, 3);
-
-	var pushToQueue = Promise.promisify(queue.push, queue);
-
-	// standard upload method
-	function upload(data, filepath, options) {
-		options = options || {};
-		var s3 = options.client || client;
-
-		if (typeof data === "string") data = new Buffer(data, "utf-8");
-		if (Buffer.isBuffer(data)) {
-			data = toStream(data);
-			if (options.size == null) options.size = data.length;
-		}
-
-		if (options.size == null) throw new Error("Missing file size. Please include in options.");
-		if (options.type == null) options.type = mime.lookup(filepath);
+	// attach appcore-file upload api
+	if (this.files) this.files.upload.register("s3", function(data, options) {
+		var filepath = options.s3path;
+		if (!filepath) throw new Error("Missing S3 upload path.");
 
 		var headers = { "Content-Length": options.size };
-		if (options.type) headers["Content-Type"] = options.type;
+		if (options.mimetype) headers["Content-Type"] = options.mimetype;
 
-		if (options.meta != null) _.each(options.meta, function(v, k) {
+		if (options.s3meta != null) _.each(options.s3meta, function(v, k) {
 			headers["x-amz-meta-" + k] = v;
 		});
 
@@ -54,26 +32,8 @@ module.exports = function() {
 					return reject(new Error("S3 upload failed with status " + res.statusCode));
 				}
 
-				resolve(req.url);
+				resolve({ url: req.url });
 			});
 		});
-	}
-
-	// vanity method for choosing queue or straight upload
-	var api = this.uploadToS3 = function(data, filepath, options) {
-		if (options && options.queue === false) return upload.apply(null, arguments);
-
-		var task = {
-			data: data,
-			path: filepath,
-			options: options
-		};
-
-		return pushToQueue(task).then(function() {
-			return task.url;
-		});
-	}
-
-	// attach knox client for access
-	_.extend(api, { knox: client });
+	});
 }
